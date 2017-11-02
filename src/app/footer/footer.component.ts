ee5e72825a84749b6dce422cb3ba9c2796c3c5b3
eragonlance <world.apocalypse@hotmail.com>
2017-11-02 19:54:15 +0700

    remember picked theme to localStorage

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,30 +1,51 @@
 import { Utility } from './../shared/utilities';
-import { Component, OnInit } from '@angular/core';
-import { MatSlideToggleChange } from '@angular/material';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+import { MatSlideToggleChange, MatSlideToggle } from '@angular/material';
+import { ChangeDetectorRef } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.styl']
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent implements OnInit, AfterViewInit {
+  @ViewChild(MatSlideToggle) slideToggle: MatSlideToggle;
   link = <HTMLLinkElement>document.querySelector('link[rel="stylesheet"]');
+  isDark = false;
+
+  constructor(private cdr: ChangeDetectorRef) {}
 
   ngOnInit() {
     this.primary = '#673ab7';
     this.accent = '#ffd740';
+
+    const theme = localStorage.getItem('theme');
+    if (theme && theme === 'dark.bundle.css') {
+      this.isDark = true;
+      this.link.href = 'dark.bundle.css';
+      this.primary = '#7b1fa2';
+      this.accent = '#69f0ae';
+    }
+  }
+
+  ngAfterViewInit() {
+    if (this.isDark) {
+      this.slideToggle.toggle();
+    }
+    this.cdr.detectChanges();
   }
 
   onSwitch(e: MatSlideToggleChange) {
-    this.link.href = e.checked ? 'dark.bundle.css' : 'light.bundle.css';
     if (e.checked) {
       this.link.href = 'dark.bundle.css';
       this.primary = '#7b1fa2';
       this.accent = '#69f0ae';
+      localStorage.setItem('theme', 'dark.bundle.css');
     } else {
       this.link.href = 'light.bundle.css';
       this.primary = '#673ab7';
       this.accent = '#ffd740';
+      localStorage.setItem('theme', 'light.bundle.css');
     }
   }
 
